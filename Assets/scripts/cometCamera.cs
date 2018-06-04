89e3f899229235f4c074cc8465ad6589b4ea0824
benzen89 <salomaa.leo@gmail.com>
2018-06-04 14:02:09 +0300

    Navigation and Commenting system added

---
+++ Assets/scripts/cometCamera.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cometCamera : MonoBehaviour {
+
+	public GameObject camOrb;
+	public GameObject mainCam;
+
+	bool rotating = false;
+	bool clicked;
+	float doubleClickTimer = -1;
+	Vector3 targetPos;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)){
+			checkDoubleClick();
+		}
+		cancelDoubleClick();
+
+		//Rotate if right mouse down
+        if (Input.GetMouseButtonDown(1))
+			rotating = true;
+		if (Input.GetMouseButtonUp(1))
+			rotating = false;
+		if(rotating){
+			Rotate();
+		}
+
+		Zoom();
+
+	}
+
+	void Rotate(){
+		float deltaX = Input.GetAxis("Mouse Y") * 2;
+		float deltaY = Input.GetAxis("Mouse X") * 2;
+
+		float rotX = camOrb.transform.rotation.eulerAngles.x;
+		float rotY = camOrb.transform.rotation.eulerAngles.y;
+
+		float toX;
+		float toY;
+
+		toX = Mathf.Clamp(rotX + deltaX, 1, 89);	//clamp so it does not go upside down or under ground
+		toY = rotY + deltaY;
+
+		Quaternion toRotation = Quaternion.Euler(toX, toY, 0);
+		camOrb.transform.rotation = toRotation;
+	}
+
+	void Zoom(){
+		float zoomDelta;
+		zoomDelta = Input.GetAxis("Mouse ScrollWheel") * 50;
+		float posZ;
+		posZ = Mathf.Clamp(mainCam.transform.localPosition.z + zoomDelta, -500, 0);
+
+		mainCam.transform.localPosition = new Vector3(0, 0, posZ);
+	}
+
+	void cancelDoubleClick(){
+		if(clicked){
+			doubleClickTimer -= Time.deltaTime;
+		}
+		if(doubleClickTimer < -0.1)
+			clicked = false;
+	}
+
+	void checkDoubleClick(){
+		if(clicked){
+			//second click
+			goToTargetPosition();
+		} else {
+			//first click
+			//get target position
+			getTargetPosition();
+			clicked = true;
+			doubleClickTimer = 1.0f;
+		}
+	}
+
+	void getTargetPosition(){
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity)){
+			targetPos = hit.point;
+		}
+	}
+
+	void goToTargetPosition(){
+		this.transform.position = targetPos;
+	}
+
+}
---
+++ Assets/scripts/commentingTool.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+public class commentingTool : MonoBehaviour {
+
+	/*
+	Make sure Player settings/Other settings / Api compatability level is set to .Net 2.0 not .Net 2.0 Subset
+	On the Gmail account, make sure "Access for less secure apps" has been turned on
+	*/
+
+	public GameObject commentBtn;
+	public GameObject commentingObjs;
+	public GameObject textInputObj;
+
+	public void StartCommenting(){
+		commentBtn.SetActive(false);
+		commentingObjs.SetActive(true);
+		ScreenCapture.CaptureScreenshot(Application.persistentDataPath+ "/screenshot.png");
+		Debug.Log(Application.persistentDataPath);
+	}
+
+	public void CancelCommenting(){
+		commentBtn.SetActive(true);
+		commentingObjs.SetActive(false);
+		Debug.Log(textInputObj.GetComponent<Text>().text);
+	}
+
+	public void SendEmailWithScreenshot(string myComment) {
+		MailMessage mail = new MailMessage();
+		mail.From = new MailAddress("tampere3dbot@gmail.com");
+		mail.To.Add("tampere3dbot@gmail.com");
+        mail.Subject = "Kommentti";
+        mail.Body = textInputObj.GetComponent<Text>().text; 							//"This is for testing SMTP mail from GMAIL"
+        string filename = "screenshot.png"; 			//"screenshot.jpg"
+        mail.Attachments.Add (new Attachment(Application.persistentDataPath+ "/"+ filename));
+
+		SmtpClient smtpServer = new SmtpClient("smtp.gmail.com");
+        smtpServer.Port = 587;
+        smtpServer.Credentials = new System.Net.NetworkCredential("tampere3dbot@gmail.com", "tiilisilakkasavurauta") as ICredentialsByHost;
+
+        smtpServer.EnableSsl = true;
+        ServicePointManager.ServerCertificateValidationCallback = 
+            delegate(object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) 
+                { return true; };
+        smtpServer.Send(mail);
+        Debug.Log("success");
+	}
+}
